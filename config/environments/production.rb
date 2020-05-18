d0a6835e9423c540bec2238aa7323f74d55fa52d
StarBuckR <ridvantulemen@gmail.com>
2020-05-18 20:04:16 +0300

    production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "api_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :mailjet
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
